51ca02bf77f078fcaa8434be2a4ef7a53c012024
Ryan Hirsch <ryan.hirsch@gmail.com>
2015-09-14 20:19:04 -0500

    Pull in Dragula

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,9 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/dragula.js/dist/dragula.js');
+  app.import('bower_components/dragula.js/dist/dragula.css');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
